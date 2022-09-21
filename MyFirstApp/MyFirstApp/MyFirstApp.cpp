663adcd500742b585f4765cd8a8b2ebc54608b67
David Faibil <100334861+davefcodes@users.noreply.github.com>
2022-09-21 01:29:28 -0400

    learning Cpp
    Learning C and C++ during my Fall semester to work on robotics projects

---
+++ MyFirstApp/MyFirstApp/MyFirstApp.cpp
@@ -0,0 +1,16 @@
+// MyFirstApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+  cout << "Hello World!\n"; // this will output to the console hello world
+  cout << "My name is David."<<endl;
+  cout << "This will be a good year\n";
+  cout << "Have a nice day";
+  cout << "Working on C++"; 
+  system("pause>0");
+}
+
